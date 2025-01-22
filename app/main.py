40fd5c91b445b5d260d18b2a33ab60855876d28e
StefanoRichi <natthaphon.a@takeitsolution.com>
2025-01-22 10:21:55 +0700

    [FIX] - RM Import

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI, HTTPException
 from starlette.middleware.cors import CORSMiddleware
-from app.core.config import settings
-from app.api.routes import api_router
+
 
 app = FastAPI(
-    title=settings.PROJECT_NAME,
-    version=settings.API_V1_STR
+    title="Todo API",
+    version="/api/v1"
 )
 
 @app.get("/")
